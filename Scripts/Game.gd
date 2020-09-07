b8f93fcdde979867236109b4c613e56a890a4403
Kaan Toprak <nkaant@googlemail.com>
2020-09-07 22:55:22 +0200

    added player bar

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -7,15 +7,27 @@ var columns = 8
 
 var objBrick_load = preload("res://Scenes/objBrick.tscn")
 
+var playerBar_load = preload("res://Scenes/playerBar.tscn")
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass 
 	windowSize = Window.rect_position
 	for rowPos in range(rows):
 		for colPos in range(columns):
 			var objBrick = objBrick_load.instance()
 			objBrick.rect_position = Vector2(colPos * 240, rowPos * 140) + Vector2(20, 20)
 			Window.add_child(objBrick)
+			
+	var playerBar = playerBar_load.instance()
+	Window.add_child(playerBar)
+
+
+
+
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
