39e14ab1f26905f401dd4fad997a167dcc981838
Kaan Toprak <nkaant@googlemail.com>
2020-09-08 23:16:27 +0200

    side and top border fix, added ball acceleration

--- Scripts/objBall.gd
+++ Scripts/objBall.gd
@@ -3,7 +3,11 @@ extends Sprite
 var moveVec 
 var rng = RandomNumberGenerator.new()
 var arc = 0.1 
-var speed = 3
+var speed = 5
+var speedMod = 1.5
+var timer = 0
+var speedCap = pow(speedMod, 3)
+
 
 func _ready():
 	rng.randomize()
@@ -11,10 +15,20 @@ func _ready():
 	moveVec = Vector2(cos(angle), -sin(angle))
 
 func _process(delta):
-	position += moveVec * speed
+	position += moveVec * speed * delta * 100
+	timer += delta
+	if timer > 5:
+		timer = 0
+		speed = speed * speedMod
+	if speed > 5 * speedCap:
+		speed = 5 * speedCap
+		
 
 func _on_Area2D_area_entered(area):
-	if area.name == "areaVertical":
+	if area.name == "areaVertical" or area.name == "areaTop":
 		moveVec[1] *= -1 
 	elif area.name == "areaHorizontal":
 		moveVec[0] *= -1
+		
+		
+		
