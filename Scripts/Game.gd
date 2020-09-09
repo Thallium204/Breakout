76a58781a241b0a668b0b4dff907882f6ff2b094
Daniel Jorden <12djorden@gmail.com>
2020-09-09 13:38:19 +0100

    added artistic flare

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -22,7 +22,7 @@ func _ready():
 		for colPos in range(columns):
 			var objBrick = objBrick_load.instance()
 			objBrick.position = Vector2(colPos * 240, rowPos * 140) + Vector2(20, 20)
-			objBrick.get_node("colorBrick").color = startingColor + (endingColor - startingColor)/(rows - 1) * rowPos
+			objBrick.get_node("colorOutline/colorBrick").color = startingColor + (endingColor - startingColor)/(rows - 1) * rowPos
 			objBrick.name = "objBrick"+str(rowPos)+str(colPos)
 			Window.add_child(objBrick)
 			print(objBrick.name)
