c424aa4b1bfd4f9e45583c5d8ab54c44f77cfd6f
Daniel Jorden <12djorden@gmail.com>
2020-09-09 13:09:36 +0100

    added gradient

--- Scripts/objBall.gd
+++ Scripts/objBall.gd
@@ -4,9 +4,7 @@ var moveVec
 var rng = RandomNumberGenerator.new()
 var arc = 0.1 
 var speed = 8
-var speedMod = 1.5
-var timer = 0
-var speedCap = pow(speedMod, 3)
+var speedCap = 16
 
 func _ready():
 	rng.randomize()
@@ -17,18 +15,12 @@ func _physics_process(delta):
 	
 	var collision_info = move_and_collide(moveVec * speed * delta * 100)
 	if collision_info: # If we've collided with something
-		
 		moveVec = moveVec.bounce(collision_info.normal) # changing our exit trajectory
-		print(collision_info.collider.name)
 		if "objBrick" in collision_info.collider.name:
 			collision_info.collider.queue_free()
 	
-	timer += delta
-	if timer > 5:
-		timer = 0
-		speed = speed * speedMod
-	if speed > 5 * speedCap:
-		speed = 5 * speedCap
+	if speed < speedCap:
+		speed += delta
 
 
 
