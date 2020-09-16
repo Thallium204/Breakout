d8d2f5f7042c6f794f9ca6f7ce5ad7ed1f930861
Kaan Toprak <nkaant@googlemail.com>
2020-09-16 16:35:41 +0200

    changed to portrait mode

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -5,6 +5,7 @@ onready var Bricks = get_node("Bricks")
 var windowSize 
 var level = 1
 var colorArray = []
+var scrDiv = 0.5
 
 export(int) var rows  
 export(int) var columns 
@@ -25,11 +26,15 @@ func _ready():
 
 func generateRow():
 	# distance above the node at which the next row of blocks is created
-	var brickHeight = level * -140
+	var brickSpacing = Vector2(rect_size.x / columns, (rect_size.y * scrDiv) / rows)
 	# instances objBrick, sets its paramters, changes its value and adds it as a child of the Bricks node
 	for colPos in range(columns):
 		var objBrick = objBrick_load.instance()
-		objBrick.position = Vector2(colPos * 240, brickHeight) + Vector2(20, 20)
+		objBrick.position = Vector2(colPos * brickSpacing.x, - level * brickSpacing.y) + Vector2(20, 20)
+		var brickSize = brickSpacing - Vector2(40, 40)
+		objBrick.get_node("CollisionShape2D").shape.radius = brickSize.y / 2
+		objBrick.get_node("CollisionShape2D").shape.height = brickSize.x - brickSize.y
+		objBrick.get_node("colorOutline").rect_size = brickSize
 		objBrick.get_node("colorOutline/colorBrick").color = colorArray[(level - 1) % colorArray.size()]
 		objBrick.name = "objBrick"+str(level)+str(colPos)
 		objBrick.value = level 
@@ -41,7 +46,7 @@ func generateRow():
 	add_child(twnBricks)
 	twnBricks.interpolate_property(
 		Bricks, "position", 
-		Vector2(0, (level-1) * 140), Vector2(0, level * 140), 
+		Vector2(0, (level-1) * brickSpacing.y), Vector2(0, level * brickSpacing.y), 
 		0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
 	)
 	twnBricks.start()
--- Scripts/objBar.gd
+++ Scripts/objBar.gd
@@ -1,8 +1,23 @@
 extends KinematicBody2D
 
+onready var colorSides = $colorSides
+
+export(Vector2) var barSize 
+
+func _ready():
+	updateBarSize()
+
 func _input(event):
 	if event is InputEventScreenDrag:
 		move_and_collide(Vector2(event.relative.x, 0))
 
 func updateUI():
-	$labCoins.text = str(Globals.score)
+	$colorSides/labCoins.text = str(Globals.score)
+	
+func updateBarSize():
+	colorSides.margin_left = - barSize.x / 2
+	colorSides.margin_right = barSize.x / 2
+	colorSides.margin_top = - barSize.y / 2
+	colorSides.margin_bottom = barSize.y / 2
+	$shapeBar.shape.radius = barSize.y / 2
+	$shapeBar.shape.height = barSize.x - barSize.y
