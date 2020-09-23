f769ec58d623c16c46a0a690278a7ccb3cb32d7f
Daniel Jorden <12djorden@gmail.com>
2020-09-23 16:43:58 +0100

    export and sensitivity

--- Scripts/objBar.gd
+++ Scripts/objBar.gd
@@ -13,7 +13,7 @@ func _ready():
 
 func _input(event):
 	if event is InputEventScreenDrag:
-		move_and_collide(Vector2(event.relative.x, 0))
+		move_and_collide(2*Vector2(event.relative.x, 0))
 
 func updateUI():
 	$colorSides/labCoins.text = str(floor(Globals.score))
